3fd56e5e425d3dbffa05dae816dc57662a98692e
Анастасия <nastyaleo98@yandex.ru>
2019-02-12 02:37:05 +0700

    1lab is done

---
+++ ConsoleApp2test/ConsoleApp2test/CalendarWriter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2test
+{
+    class CalendarWriter
+    {
+        private const String Title = "sun mon tue wed thu fri sat\n";
+        private int _spaces = 4;
+
+        public void Write(DateTime dateValue)
+        {
+            StringBuilder calendar = new StringBuilder(Title);
+            var workingDays = 0;
+            var date = new DateTime(dateValue.Year, dateValue.Month, 1);
+            WriteAlignmentBeforeFirstDate(date, calendar);
+            for (; date.Month == dateValue.Month; date = date.AddDays(1))
+            {
+                WriteDateWithAlignment(date, calendar);
+                if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    ++workingDays;
+                }
+            }
+            Console.WriteLine(calendar);
+            Console.WriteLine("Working days count: {0}", workingDays);
+        }
+
+        private void WriteAlignmentBeforeFirstDate(DateTime date, StringBuilder calendar)
+        {
+            int dayOfWeek = (int)date.DayOfWeek;
+            calendar.Append(String.Join("", Enumerable.Repeat(" ", dayOfWeek * _spaces)));
+        }
+
+        private void WriteDateWithAlignment(DateTime date, StringBuilder calendar)
+        {
+            calendar.Append(date.Day);
+            calendar.Append(date.DayOfWeek == DayOfWeek.Saturday
+                ? "\n"
+                : String.Join("", Enumerable.Repeat(" ", _spaces - (int) Math.Floor(Math.Log10(date.Day) + 1))));
+        }
+
+
+    }
+}
---
+++ ConsoleApp2test/ConsoleApp2test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2test
+{
+    class Program
+    {
+        private Dictionary<int, int> f = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write date");
+            var dateString = Console.ReadLine();
+
+            if (!DateTime.TryParse(dateString, out var dateValue))
+            {
+                Console.WriteLine("Couldn't parse date");
+                return;
+            }
+            var writer = new CalendarWriter();
+            writer.Write(dateValue);
+      //      Console.ReadLine();
+        }
+    
+    }
+}
