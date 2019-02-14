bb561970dbd83fbf019526b9cf0d89f8d1824de4
Анастасия <nastyaleo98@yandex.ru>
2019-02-14 17:51:09 +0700

    first version

---
+++ LinesCounter/LinesCounter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Bad number of arguments. Write one type of files you want to check.");
+                return;
+            }
+
+            var type = args[0];
+            var currentPath = Directory.GetCurrentDirectory();
+            var files = Directory.GetFiles("C:\\Users\\Анастасия\\source\\repos\\csharp-course\\LinesCounter", type, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var totalCount = 0;
+                var usefulCount = 0;
+                var fileStream = new FileStream($"{file}", FileMode.Open, FileAccess.Read);
+                var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    ++totalCount;
+                    if (!line.StartsWith("//") && string.IsNullOrWhiteSpace(line))
+                    {
+                        ++usefulCount;
+                    }
+                }
+                Console.WriteLine($"For {file}:\ntotal lines - {totalCount}\nuseful lines - {usefulCount}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
